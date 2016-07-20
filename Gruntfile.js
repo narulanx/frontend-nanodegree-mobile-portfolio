c4d46cfc2ea138522578fe0acefc187b087c2241
Nikhil Narula <nik.coolest@gmail.com>
2016-07-20 01:00:55 -0400

    Add gruntfile.js and package.json

---
+++ Gruntfile.js
@@ -0,0 +1,70 @@
+module.exports = function(grunt) {
+
+// var mozjpeg = require('imagemin-mozjpeg');
+
+  grunt.initConfig({
+    responsive_images: {
+      dev: {
+        options: {
+          engine: 'im',
+          sizes: [{
+            width: 116,
+            height: 75,
+            suffix: '_small'
+          }]
+        },
+        files: [{
+          expand: true,
+          src: ['pizzeria.jpg'],
+          cwd: 'views/images/',
+          dest: 'views/resized_images/'
+        }]
+      }
+    },
+
+    imagemin: {
+      jpg: {
+        options: {
+          progressive: true
+        },
+        files: [{
+          expand: true,
+          cwd: 'views/resized_images/',
+          src: ['*.jpg'],
+          dest: 'views/images/',
+          ext: '.jpg'
+        }]
+      }
+    },
+
+    cssmin: {
+      target: {
+        files: [{
+          expand: true,
+          cwd: 'css',
+          src: ['style.css'],
+          dest: 'css',
+          ext: '.min.css'
+        }]
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      my_target: {
+        files: {
+          'views/js/main.min.js': ['views/js/main.js']
+        }
+      }
+    }
+  });
+
+  grunt.loadNpmTasks('grunt-responsive-images');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.registerTask('default', ['uglify','cssmin','imagemin','responsive_images']);
+
+};
