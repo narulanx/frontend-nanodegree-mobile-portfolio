554f84a67f0315d07d416efcf025e677470228f5
Nikhil Narula <nik.coolest@gmail.com>
2016-07-20 23:59:04 -0400

    Assign randomPizzaContainer selector to a variable

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,11 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var pizzaContainerSelector = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < pizzaContainerSelector.length; i++) {
+      var dx = determineDx(pizzaContainerSelector[i], size);
+      var newwidth = (pizzaContainerSelector[i].offsetWidth + dx) + 'px';
+      pizzaContainerSelector[i].style.width = newwidth;
     }
   }
 
