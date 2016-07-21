62dbd8caea7c267f0fd00c243bbdeaedd786a00d
Nikhil Narula <nik.coolest@gmail.com>
2016-07-21 00:07:33 -0400

    Change method to recalculate pizza width

--- views/js/main.js
+++ views/js/main.js
@@ -421,39 +421,26 @@ var resizePizzas = function(size) {
 
   changeSliderLabel(size);
 
-   // Returns the size difference to change a pizza element from one size to another. Called by changePizzaSlices(size).
-  function determineDx (elem, size) {
-    var oldWidth = elem.offsetWidth;
-    var windowWidth = document.querySelector("#randomPizzas").offsetWidth;
-    var oldSize = oldWidth / windowWidth;
-
-    // Changes the slider value to a percent width
-    function sizeSwitcher (size) {
-      switch(size) {
+   // Iterates through pizza elements on the page and changes their widths
+  function changePizzaSizes(size) {
+    var pizzaContainerSelector = document.querySelectorAll(".randomPizzaContainer");
+    var newwidth;
+    switch(size) {
         case "1":
-          return 0.25;
+          newwidth = 25;
+          break;
         case "2":
-          return 0.3333;
+          newwidth = 33.33;
+          break;
         case "3":
-          return 0.5;
+          newwidth = 50;
+          break;
         default:
           console.log("bug in sizeSwitcher");
       }
-    }
 
-    var newSize = sizeSwitcher(size);
-    var dx = (newSize - oldSize) * windowWidth;
-
-    return dx;
-  }
-
-  // Iterates through pizza elements on the page and changes their widths
-  function changePizzaSizes(size) {
-    var pizzaContainerSelector = document.querySelectorAll(".randomPizzaContainer");
     for (var i = 0; i < pizzaContainerSelector.length; i++) {
-      var dx = determineDx(pizzaContainerSelector[i], size);
-      var newwidth = (pizzaContainerSelector[i].offsetWidth + dx) + 'px';
-      pizzaContainerSelector[i].style.width = newwidth;
+      pizzaContainerSelector[i].style.width = newwidth + '%';
     }
   }
 
